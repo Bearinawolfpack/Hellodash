12e745186d4c2e71029454dfd826cbf177abfa80
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-24 15:45:18 -0400

    FindIdentity roughed in

--- src/Components/FunctionComponents/Util/FindIdentity.js
+++ src/Components/FunctionComponents/Util/FindIdentity.js
@@ -1,26 +1,59 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 class FindIdentity extends React.Component {
   state = {
     showResult: false,
+    anyValue: '',
   };
 
   render() {
-    const Identity = () => {
-      return 'This is the identity function';
+    const Identity = (value) => {
+      console.log(value);
+      return <h4>{value}</h4>;
     };
 
     const submitHandler = (event) => {
-      Identity();
+      event.preventDefault();
+      this.setState({ showResult: true });
     };
 
+    const resetHandler = () => {
+      this.setState({ showResult: false, anyValue: '' });
+    };
+
+    const changeHandler = (event) => {
+      this.setState({ [event.target.name]: event.target.value });
+    };
+
+    const showResult = this.state.showResult;
+
     return (
-      <div>
-        <button type="submit" onClick={submitHandler}>
-          Identity
-        </button>
-        <p>{Identity()}</p>
-      </div>
+      <Container>
+        <Form onSubmit={submitHandler} onReset={resetHandler}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              name="anyValue"
+              type="text"
+              placeholder="Enter Any Value"
+              onChange={changeHandler}
+              value={this.state.anyValue}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+          <Button variant="primary" type="reset">
+            Reset
+          </Button>
+        </Form>
+        {this.state.showResult
+          ? Identity(this.state.anyValue)
+          : "Type and Value then hit 'Submit'"}
+      </Container>
     );
   }
 }
